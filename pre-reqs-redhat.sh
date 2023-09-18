cb714e006de9d1a1d74026ed60c05e4016078c10
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-09-18 17:12:23 -0300

    va

---
+++ pre-reqs-redhat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+yum update -y
+
+sudo yum install -y dos2unix
+
+sudo yum install -y nano
+
+sudo yum install git -y
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# Enable and start docker service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+#Add user to docker group
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
