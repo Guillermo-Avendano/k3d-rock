f1aa8c93186f855aa1eb7e1b32efaeeda0acef3d
Guillermo-Avendano <avendano.guillermo@gmail.com>
2023-11-01 17:49:31 -0300

    va

--- inputframework/image/MobiusRemoteCLIsmall.sh
+++ inputframework/image/MobiusRemoteCLIsmall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=/opt/asg/java/bin:$PATH
-
+MOBIUS_REMOTE_CLI_PATH=/opt/MobiusRemoteCLI
 java -cp "$MOBIUS_REMOTE_CLI_PATH/BOOT-INF/classes:$MOBIUS_REMOTE_CLI_PATH/BOOT-INF/lib/*" com.asg.mobiuscli.MobiusCliApplication $@
--- inputframework/image/acreate.sh
+++ inputframework/image/acreate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=/opt/asg/java/bin:$PATH
-
+MOBIUS_REMOTE_CLI_PATH=/opt/MobiusRemoteCLI
 java -D"log4j.configurationFile=$MOBIUS_REMOTE_CLI_PATH/BOOT-INF/classes/log4j2.yaml" -jar ${MOBIUS_REMOTE_CLI_PATH}/acreate-cli.jar acreate $@
