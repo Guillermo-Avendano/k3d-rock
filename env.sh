1791da7c5c8795c8562c7b178d024cc376243659
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-11-07 09:00:45 -0300

    va

--- env.sh
+++ env.sh
@@ -63,7 +63,7 @@ export KUBE_ISTIO_ENABLED="false"
 export KUBE_NFS_ENABLED=${KUBE_NFS_ENABLED:-false}
 
 export KUBE_STORAGE_CLASS=local-path
-export KUBE_STORAGE_READ_WRITE=ReadWriteOnly
+export KUBE_STORAGE_READ_WRITE=ReadWriteOnce
 
 
 
