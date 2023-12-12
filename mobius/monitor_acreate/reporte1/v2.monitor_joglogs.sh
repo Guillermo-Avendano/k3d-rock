7cd52e3978cc7af877812610b5308078e9709051
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-12-12 13:17:49 -0300

    va

--- mobius/monitor_acreate/reporte1/v2.monitor_joglogs.sh
+++ mobius/monitor_acreate/reporte1/v2.monitor_joglogs.sh
@@ -3,8 +3,11 @@
 SEGUNDOS_ESPERA=10
 while true; do
 
+  # Consolida DDs de un mismo JOB
+  ./v2.consolida.sh
+
   # Ejecutar el script principal
-  ./acreate.sh
+  ./v2.acreate.sh
 
   # "$SEGUNDOS_ESPERA" antes de la próxima ejecución
   sleep $SEGUNDOS_ESPERA
