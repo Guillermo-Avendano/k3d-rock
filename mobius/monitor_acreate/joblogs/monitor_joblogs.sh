d031a4771672ce1d4534818047f88e41eba7a893
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-12-19 10:01:11 -0300

    va

--- mobius/monitor_acreate/joblogs/monitor_joblogs.sh
+++ mobius/monitor_acreate/joblogs/monitor_joblogs.sh
@@ -3,7 +3,7 @@
 SEGUNDOS_ESPERA=10
 while true; do
   # Ejecutar el script principal
-  ./acreate.sh
+  ./acreate.v2.sh
 
   # "$SEGUNDOS_ESPERA" antes de la próxima ejecución
   sleep $SEGUNDOS_ESPERA
