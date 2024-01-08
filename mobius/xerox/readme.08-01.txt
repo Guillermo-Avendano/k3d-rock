52c5ec54c4e2932504203905aff6ff83d20da559
Guillermo Avendano <gavendano@rocketsoftware.com>
2024-01-08 14:50:00 -0300

    va

---
+++ mobius/xerox/readme.08-01.txt
@@ -0,0 +1,1 @@
+./MobiusRemoteCLI.sh acreate -s Mobius -u ADMIN -f ../k3d-rock/pv_cluster/mobius-storage/externalresources/MCOPT105S/INPUT.D010324.RDW -r MCOPT105S -c MCOPT105S -v 2
--- mobius/xerox/txt2rdw.py
+++ mobius/xerox/txt2rdw.py
@@ -1,24 +1,47 @@
 import sys
 
-#fin=sys.argv[1]
-#fout=sys.argv[2]
+def convert1(input_file_path, output_file_path):
+	# Chunk size for reading the input file
+	chunk_size = 32760  # Adjust this size based on your needs
 
-fin="C:/Rocket.Git/k3d-rock/mobius/xerox/vader111.datos.txt"
-fout="C:/Rocket.Git/k3d-rock/mobius/xerox/vader111.datos.rdw"
+	# Open input and output files
+	with open(input_file_path, 'r', encoding='ANSI') as fi, open(output_file_path, 'wb') as fo:
+		while True:
+			chunk = fi.read(chunk_size)
+			if not chunk:
+				break
 
-#fin="C:/Rocket.Git/k3d-rock/mobius/xerox/MCOPT105.TXT"
-#fout="C:/Rocket.Git/k3d-rock/mobius/xerox/MCOPT105.RDW"
+			# Process each chunk
+			for line in chunk.splitlines():
+				res = line.rstrip()
+				ll = len(res) + 4
+				res_ebc = res.encode('cp500')
+				if res == '\x1a':
+					res_ebc = b'\x1c'
+				rdw = ll.to_bytes(2, byteorder='big') + b'\x00\x00'
 
-fo=open(fout,'wb')
-with open(fin,'rb') as fi:
-	while 1:
-		line=fi.readline()
-		if not line:break
-		res=line.rstrip()
-		ll=len(res)+4
-		res_ebc=res.decode('ANSI').encode('cp500')
-		if res==b'\x1a':
-			res_ebc=b'\x1c'
-		rdw=bytes(chr(int(ll/256))+chr(ll%256)+'\x00\x00',encoding='ANSI')
-		fo.write(rdw+res_ebc)
-fo.close()
+				# Write RDW followed by the record to the output file
+				fo.write(rdw + res_ebc)
+
+	print("Conversion completed.")
+
+def convert_chema(fin, fout):
+	fo=open(fout,'wb')
+	with open(fin,'rb') as fi:
+		while 1:
+			line=fi.readline()
+			if not line:break
+			res=line.rstrip()
+			ll=len(res)+4
+			res_ebc=res.decode('ANSI').encode('cp500')
+			if res==b'\x1a':
+				res_ebc=b'\x1c'
+			rdw=bytes(chr(int(ll/256))+chr(ll%256)+'\x00\x00',encoding='ANSI')
+			fo.write(rdw+res_ebc)
+	fo.close()
+
+
+fin="C:/Rocket.Git/k3d-rock/mobius/xerox/MCOPT105.TXT"
+fout="C:/Rocket.Git/k3d-rock/mobius/xerox/MCOPT105.RDW"
+
+convert1 (fin, fout)
